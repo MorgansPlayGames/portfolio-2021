820ad21c543b88a4d587a7bf77a15c4ddba82f4b
Parker Morgan <jpmaster13@msn.com>
2021-02-11 20:52:30 -0700

    Added Biography base html no formatting

---
+++ src/components/Bio.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Bio() {
+    return (
+        <section>
+            <h3>About Me:</h3>
+            <p>Hello! I am Parker, I'm full stack developer.</p>
+            <p>I </p>
+            <p>I love finding simple solutions to complex problems.</p>
+            
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>JQuery</li>
+                <li>React</li>
+                <li>Handlebars</li>
+                <li>Node</li>
+                <li>Bootstrap</li>
+                <li>Mongo</li>
+                <li>Sequelize</li>
+                <li>Express</li>
+                <li>Ajax</li>
+                <li>Axios</li>
+            </ul>
+            <p>Cooking is my hobby.</p>
+            <p>I also run tabletop roleplaying games</p>
+        </section>
+    );
+}
+
+export default Bio;
