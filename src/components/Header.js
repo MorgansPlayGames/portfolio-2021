74c18d04d55f62e99b11554dd0f4e820e032fbd7
Parker Morgan <jpmaster13@msn.com>
2021-02-11 20:55:34 -0700

    Header base

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h3>Parker Morgan</h3>
+            <p>From server to browser,</p>
+            <p>Coding is an art to practice.</p>
+            <p>Here has been my journey:</p>
+        </header>
+    );
+}
+
+export default Header;
