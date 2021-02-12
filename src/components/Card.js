6cbb1e0ae4c288588ff5088c7172e3682a57eef5
Parker Morgan <jpmaster13@msn.com>
2021-02-11 20:53:38 -0700

    Added a card base for html with placeholders

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card(props) {
+//console.log(props)
+  return (
+    <div className="card">
+        <h5>Project Title</h5>
+        <img src="https://via.placeholder.com/150" />
+        <a>View Project</a>
+    </div>
+    
+     
+  );
+}
+
+export default Card;
