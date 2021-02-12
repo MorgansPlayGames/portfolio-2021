a9bb06ee3c240b26788725beb956ad490a1d103c
Parker Morgan <jpmaster13@msn.com>
2021-02-11 20:54:56 -0700

    Added a button for git link that can be placed throughout

---
+++ src/components/GitButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function GitButton() {
+    return (
+        <a href="https://github.com/MorgansPlayGames" target="_blank">MorgansPlayGames Github</a>
+    )
+}
+
+export default GitButton;
