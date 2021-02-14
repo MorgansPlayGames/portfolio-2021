37ec484b030eb11bede62dca4992f2e5c6fbc778
Parker Morgan <jpmaster13@msn.com>
2021-02-13 22:34:44 -0700

    Tailwind updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        phone: '320px',
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1280px',
+        '2xl': '1536px',
+      },
+    },
   },
   variants: {
     extend: {},
