b245a0c5161aae39cf80463c890cc25322f2626e
Parker Morgan <jpmaster13@msn.com>
2021-02-13 22:36:27 -0700

    updated the base css

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Header from "./components/Header";
 import Bio from "./components/Bio"
 import Footer from "./components/Footer"
 
+
 function App() {
   return (
-     <div className="App">
+     <div className="App bg-gray-900 text-gray-200">
     
       <Header />
       <body>
+      <section className="container flex-wrap">
       <Card /><Card /><Card /><Card /><Card /><Card />
+      </section>
       <Bio />
     
       </body>
