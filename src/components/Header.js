8887f9b9388ef670a596968d78ed901480676c7f
Parker Morgan <jpmaster13@msn.com>
2021-02-17 00:13:16 -0700

    Added a timed animation to the header statement

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 function Header() {
-    return (
-        <header className="p-8 m-4">
-            <h2 className="text-8xl py-36">Parker Morgan</h2>
-            <h3 className="text-4xl py-36">From server to browser,</h3>
-            <h3 className="text-4xl py-36">Coding is an art to practice.</h3>
-            <h2 className="text-4xl py-36">Here has been my journey:</h2>
-        </header>
-    );
+  return (
+    <header className="p-8 m-4">
+      <h2 className="text-8xl py-36 hiddenAnimate">Parker Morgan</h2>
+      <h3 className="text-4xl py-36 hiddenAnimate animate__fadeInUp animate__animated animate__delay-2s">
+        From server to browser,
+      </h3>
+      <h3 className="text-4xl py-36 hiddenAnimate animate__fadeInUp animate__animated animate__delay-3s">
+        Coding is an art to practice.
+      </h3>
+      <h3 className="text-4xl py-36 hiddenAnimate animate__fadeInUp animate__animated animate__delay-4s">
+        Here has been my journey:
+      </h3>
+    </header>
+  );
 }
 
 export default Header;
