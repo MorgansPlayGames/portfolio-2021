958ecf3f8fcbb301dd6d0e596b70cb292d7d20e9
Parker Morgan <jpmaster13@msn.com>
2021-02-17 00:19:46 -0700

    json database of project list

---
+++ src/components/lib/projectList.js
@@ -0,0 +1,31 @@
+const projList = [
+    {
+        "id":0,
+        "name":"Parker's Quiz",
+        "description":"Quiz with a timer and local host high score page",
+        "gitlink":"https://github.com/MorgansPlayGames/parkers-quiz",
+        "livesite":"https://morgansplaygames.github.io/parkers-quiz/index.html",
+        "technologies":["Bootstrap"],
+        "image":"https://github.com/MorgansPlayGames/parkers-quiz/raw/main/Screenshot.png"
+    },
+    {
+        "id":1,
+        "name":"Parker's Teampage Generator",
+        "description":"Uses command line prompts to build a team html page",
+        "gitlink":"https://github.com/MorgansPlayGames/parkers-teampage-gen",
+        "livesite":"#",
+        "technologies":["Node", "Javascript"],
+        "image":"https://github.com/MorgansPlayGames/parkers-teampage-gen/raw/main/images/example.png"
+    },
+    {
+        "id":2,
+        "name":"test",
+        "description":"test",
+        "gitlink":"test",
+        "livesite":"test",
+        "technologies":"test",
+        "image":"test"
+    }   
+]
+
+export default projList;
