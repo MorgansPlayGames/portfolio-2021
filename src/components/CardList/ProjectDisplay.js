46546767a8cf115318305269270b9aecc019f852
Parker Morgan <jpmaster13@msn.com>
2021-02-18 16:15:00 -0700

    made this a component to display under projects

---
+++ src/components/CardList/ProjectDisplay.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function ProjectDisplay(props) {
+  const checkLiveButton = () => {
+    if (props.livesite === "#") {
+      return;
+    } else {
+      return <a href={props.livesite}>Livesite</a>;
+    }
+  };
+
+  return (
+    <section className="project-display">
+      <h3 className="title">{props.project.name}</h3>
+      <p className="description">Description: {props.project.description}</p>
+      <img src={props.project.image} />
+
+      <h5 className="tech-used">Technology Used:</h5>
+      {props.project.technologies.map((tech, index) => (
+        <div key={index}>{tech + ", "}</div>
+      ))}
+
+      <a href={props.gitlink}>github repo</a>
+      {checkLiveButton}
+    </section>
+  );
+}
+
+export default ProjectDisplay;
