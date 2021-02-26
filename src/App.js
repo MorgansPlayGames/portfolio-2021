2b5c87f5aa23408a14e7ad9c5bf30786fdd08b03
Parker Morgan <jpmaster13@msn.com>
2021-02-26 12:09:42 -0700

    Changed to router with home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
-import Header from "./components/Header";
-import Bio from "./components/Bio";
+import Navbar from "./components/Navbar"
 import Footer from "./components/Footer";
-import CardList from "./components/CardList";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from "./pages/Home"; 
+import Contact from "./pages/Contact"
 
 function App() {
   return (
     <div className="App bg-gray-900 text-gray-200">
-      <Header />
-      <CardList />
-      <Bio />
+      
+      <Router>
+      <Navbar />
+      <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+      </div>
+      </Router>
       <Footer />
     </div>
   );
