e4caf6bb176ca9d876ea4845484a820b958bf3cf
Parker Morgan <jpmaster13@msn.com>
2021-02-26 12:13:03 -0700

    Added Linkedin button in addition to the github link

--- src/components/GitButton.js
+++ src/components/GitButton.js
@@ -2,9 +2,22 @@ import React from "react";
 
 function GitButton() {
   return (
-    <a href="https://github.com/MorgansPlayGames" target="_blank">
-      MorgansPlayGames Github
-    </a>
+    <>
+      <a
+        href="https://github.com/MorgansPlayGames"
+        target="_blank"
+        className="bg-green-900 rounded-lg p-2 m-2"
+      >
+        Github
+      </a>
+      <a
+        href="https://www.linkedin.com/in/parker-morgan-18ab5b1b4"
+        target="_blank"
+        className="bg-green-900 rounded-lg p-2 m-2"
+      >
+        Linkedin
+      </a>
+    </>
   );
 }
 
