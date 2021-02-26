e2b6f6be7c04fa04ab187f2520ca14189c7f8e3b
Parker Morgan <jpmaster13@msn.com>
2021-02-26 12:26:32 -0700

    Updated project list

--- src/components/lib/projectList.js
+++ src/components/lib/projectList.js
@@ -1,31 +1,83 @@
 const projList = [
-    {
-        "id":0,
-        "name":"Parker's Quiz",
-        "description":"Quiz with a timer and local host high score page",
-        "gitlink":"https://github.com/MorgansPlayGames/parkers-quiz",
-        "livesite":"https://morgansplaygames.github.io/parkers-quiz/index.html",
-        "technologies":["Bootstrap"],
-        "image":"https://github.com/MorgansPlayGames/parkers-quiz/raw/main/Screenshot.png"
-    },
-    {
-        "id":1,
-        "name":"Parker's Teampage Generator",
-        "description":"Uses command line prompts to build a team html page",
-        "gitlink":"https://github.com/MorgansPlayGames/parkers-teampage-gen",
-        "livesite":"#",
-        "technologies":["Node", "Javascript"],
-        "image":"https://github.com/MorgansPlayGames/parkers-teampage-gen/raw/main/images/example.png"
-    },
-    {
-        "id":2,
-        "name":"test",
-        "description":"test",
-        "gitlink":"test",
-        "livesite":"test",
-        "technologies":["test"],
-        "image":"test"
-    }   
-]
+  {
+    id: 0,
+    name: "Character Sheet Generator (Second Group Project)",
+    description: "Project two. Save character sheets in a database and access them anywhere with internet. Signup and login.",
+    gitlink: "https://github.com/jasmineteters/Character-Sheet-Generator",
+    livesite: "https://charactersheet-generator.herokuapp.com/",
+    technologies: [
+      "mysql",
+      "sequelize",
+      "express",
+      "bcript",
+      "handlebars",
+      "dotenv",
+    ],
+    image:
+      "https://github.com/jasmineteters/Character-Sheet-Generator/raw/main/public/imgs/appScreenshot.jpg",
+  },
+  {
+    id: 1,
+    name: "Parker's Googlebooks!",
+    description: "Full Stack app to search books from the googlebooks api and save them to a public mongo server",
+    gitlink: "https://github.com/MorgansPlayGames/parkers-googlebooks",
+    livesite: "https://parkers-googlebooks.herokuapp.com/",
+    technologies: ["HTML", "CSS", "React", "Bootstrap", "Javascript", "Mongo", "Node", "Axios"],
+    image:
+      "https://github.com/MorgansPlayGames/parkers-googlebooks/blob/main/repo_stuff/googlebooksearch.png",
+  },
+  {
+    id: 2,
+    name: "Parker's Quiz",
+    description: "Quiz with a timer and local host high score page",
+    gitlink: "https://github.com/MorgansPlayGames/parkers-quiz",
+    livesite: "https://morgansplaygames.github.io/parkers-quiz/index.html",
+    technologies: ["Bootstrap"],
+    image:
+      "https://github.com/MorgansPlayGames/parkers-quiz/raw/main/Screenshot.png",
+  },
+  {
+    id: 3,
+    name: "Parker's Teampage Generator",
+    description: "Uses command line prompts to build a team html page",
+    gitlink: "https://github.com/MorgansPlayGames/parkers-teampage-gen",
+    livesite: "#",
+    technologies: ["Node", "Javascript"],
+    image:
+      "https://github.com/MorgansPlayGames/parkers-teampage-gen/raw/main/images/example.png",
+  },
+  {
+    id: 4,
+    name: "Parker's AntiLazy",
+    description:
+      "Create a backend with a mongo database to save and store workouts, which display in charts",
+    gitlink: "https://github.com/jasmineteters/Character-Sheet-Generator",
+    livesite: "https://charactersheet-generator.herokuapp.com/",
+    technologies: ["Javascript", "Mongo", "Mongoose", "Express", "Morgan"],
+    image:
+      "https://github.com/MorgansPlayGames/parkers-antilazy/raw/main/images/Screenshot2.png",
+  },
+  {
+    id: 5,
+    name: "Parker's Employee Tracker",
+    description:
+      "Terminal that will interact with an employeedb to add modify and remove employees, roles, and departments",
+    gitlink: "https://github.com/MorgansPlayGames/parkers-employee-tracker",
+    livesite: "#",
+    technologies: ["Javascript", "SQL", "Inquirer"],
+    image:
+      "https://github.com/MorgansPlayGames/parkers-employee-tracker/raw/main/images/Screenshot1.png",
+  },
+  {
+    id: 6,
+    name: "Valence (first project)",
+    description: "Search by tag music API",
+    gitlink: "https://github.com/MorgansPlayGames/music-by-theme",
+    livesite: "https://morgansplaygames.github.io/music-by-theme/",
+    technologies: ["HTML", "CSS", "JQuery", "Bootstrap"],
+    image:
+      "https://user-images.githubusercontent.com/69328646/97086800-c497b180-15e2-11eb-9409-f179dde87fb4.jpg",
+  },
+];
 
 export default projList;
