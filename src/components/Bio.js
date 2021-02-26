27cc61d39ac1e89d77cc3273a8bd34132cdc0286
Parker Morgan <jpmaster13@msn.com>
2021-02-26 12:11:15 -0700

    Added experience randomize button, and about me

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import RandomSpot from "./randomSpot";
 
 function Bio() {
+  const experienceIn = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "JQuery",
+    "React",
+    "Handlebars",
+    "Node",
+    "Bootstrap",
+    "Tailwind",
+    "Sequelize",
+    "Mongo",
+    "MySQL",
+    "Express",
+    "Ajax",
+    "Axios",
+  ];
+  const [experienceLayout, setExperienceLayout] = useState(
+    experienceIn.map((x) => <li className={RandomSpot()}>{x}</li>)
+  );
+
+  const changeLayout = () => {
+    setExperienceLayout(
+      experienceIn.map((x) => <li className={RandomSpot()}>{x}</li>)
+    );
+  };
+
   return (
-    <section className="">
-      <p className="m-16 text-2xl">
-        Hello! I am Parker, I'm full stack developer.
-      </p>
-      <p className="m-16 text-2xl">
-        I can create a website, or update an existing one.
-      </p>
-      <p className="m-16 text-2xl">
-        I love finding simple solutions to complex problems.
-      </p>
-      <p className="m-8">Experience in:</p>
-      <ul className="md:mx-12">
-        <li className={RandomSpot()}>HTML</li>
-        <li className={RandomSpot()}>CSS</li>
-        <li className={RandomSpot()}>Javascript</li>
-        <li className={RandomSpot()}>JQuery</li>
-        <li className={RandomSpot()}>React</li>
-        <li className={RandomSpot()}>Handlebars</li>
-        <li className={RandomSpot()}>Node</li>
-        <li className={RandomSpot()}>Bootstrap</li>
-        <li className={RandomSpot()}>Mongo</li>
-        <li className={RandomSpot()}>Sequelize</li>
-        <li className={RandomSpot()}>Express</li>
-        <li className={RandomSpot()}>Ajax</li>
-        <li className={RandomSpot()}>Axios</li>
-      </ul>
-      <h3 className="m-16 text-2xl">About Me:</h3>
-      <p className="my-16 text-lg">Love to cook</p>
-      <p className="my-16 text-lg">I run tabletop games</p>
-      <p className="my-16 text-lg">Hobbiest musician</p>
-      <p className="my-16 text-lg">Enjoy the tedium of editing videos</p>
-      <p className="my-16 text-md">Casual Videogamer</p>
+    <section className="m-auto">
+      <section>
+        <p className="m-16 text-2xl">
+          Hello! I am Parker, I'm full stack developer.
+        </p>
+        <p className="m-16 text-2xl">
+          I can create a website, or update an existing one.
+        </p>
+        <p className="m-16 text-2xl">
+          I love finding simple solutions to complex problems.
+        </p>
+      </section>
+      <section>
+        <p className="m-8">Experience in:</p>
+        <ul className="lg:mx-24">{experienceLayout}</ul>
+        <button
+          className="m-2 p-2 bg-gray-700 rounded-lg"
+          onClick={changeLayout}
+        >
+          Change the Layout
+        </button>
+      </section>
+
+      <section className="w-1/2 m-auto">
+        <h3 className="m-16 text-2xl bg-gray-800 rounded-lg">About Me:</h3>
+        <p className="my-16 text-lg bg-gray-800 rounded-lg">Love to cook</p>
+        <p className="my-16 text-lg bg-gray-800 rounded-lg">
+          I run tabletop games
+        </p>
+        <p className="my-16 text-lg bg-gray-800 rounded-lg">
+          Hobbiest musician
+        </p>
+        <p className="my-16 text-lg bg-gray-800 rounded-lg">
+          Enjoy the tedium of editing videos
+        </p>
+        <p className="my-16 text-md bg-gray-800 rounded-lg">
+          Casual Videogamer
+        </p>
+      </section>
     </section>
   );
 }
