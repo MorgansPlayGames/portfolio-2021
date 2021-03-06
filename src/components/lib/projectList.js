2741ca58d482050ce3c90644ba7f875825826057
Parker Morgan <jpmaster13@msn.com>
2021-03-05 20:23:19 -0700

    Added latest project without picture

--- src/components/lib/projectList.js
+++ src/components/lib/projectList.js
@@ -1,6 +1,29 @@
 const projList = [
+
   {
-    id: 0,
+    id:0,
+    name: "Pixi (Third Group Project)",
+    description: "Project Three. E-commerse shop where users can buy and sell digital photography.",
+    gitlink: "https://github.com/ajlaursen/Pixi",
+    livesite: "https://pixi.pics.com/",
+    technologies: [
+      "mongo",
+      "jsonwebtoken",
+      "express",
+      "AWS",
+      "React",
+      "Dotenv",
+      "Axios",
+      "Bcript",
+      "Cors",
+      "Mongoose",
+      "Tailwind",
+      "Passport"
+    ],
+    image:""
+  },
+  {
+    id: 1,
     name: "Character Sheet Generator (Second Group Project)",
     description: "Project two. Save character sheets in a database and access them anywhere with internet. Signup and login.",
     gitlink: "https://github.com/jasmineteters/Character-Sheet-Generator",
@@ -17,17 +40,17 @@ const projList = [
       "https://github.com/jasmineteters/Character-Sheet-Generator/raw/main/public/imgs/appScreenshot.jpg",
   },
   {
-    id: 1,
+    id: 2,
     name: "Parker's Googlebooks!",
     description: "Full Stack app to search books from the googlebooks api and save them to a public mongo server",
     gitlink: "https://github.com/MorgansPlayGames/parkers-googlebooks",
     livesite: "https://parkers-googlebooks.herokuapp.com/",
     technologies: ["HTML", "CSS", "React", "Bootstrap", "Javascript", "Mongo", "Node", "Axios"],
     image:
-      "https://github.com/MorgansPlayGames/parkers-googlebooks/blob/main/repo_stuff/googlebooksearch.png",
+      "https://github.com/MorgansPlayGames/parkers-googlebooks/blob/main/repo_stuff/googlebooksearch.png?raw=true",
   },
   {
-    id: 2,
+    id: 3,
     name: "Parker's Quiz",
     description: "Quiz with a timer and local host high score page",
     gitlink: "https://github.com/MorgansPlayGames/parkers-quiz",
@@ -37,7 +60,7 @@ const projList = [
       "https://github.com/MorgansPlayGames/parkers-quiz/raw/main/Screenshot.png",
   },
   {
-    id: 3,
+    id: 4,
     name: "Parker's Teampage Generator",
     description: "Uses command line prompts to build a team html page",
     gitlink: "https://github.com/MorgansPlayGames/parkers-teampage-gen",
@@ -47,7 +70,7 @@ const projList = [
       "https://github.com/MorgansPlayGames/parkers-teampage-gen/raw/main/images/example.png",
   },
   {
-    id: 4,
+    id: 5,
     name: "Parker's AntiLazy",
     description:
       "Create a backend with a mongo database to save and store workouts, which display in charts",
@@ -58,7 +81,7 @@ const projList = [
       "https://github.com/MorgansPlayGames/parkers-antilazy/raw/main/images/Screenshot2.png",
   },
   {
-    id: 5,
+    id: 6,
     name: "Parker's Employee Tracker",
     description:
       "Terminal that will interact with an employeedb to add modify and remove employees, roles, and departments",
@@ -69,7 +92,7 @@ const projList = [
       "https://github.com/MorgansPlayGames/parkers-employee-tracker/raw/main/images/Screenshot1.png",
   },
   {
-    id: 6,
+    id: 7,
     name: "Valence (first project)",
     description: "Search by tag music API",
     gitlink: "https://github.com/MorgansPlayGames/music-by-theme",
