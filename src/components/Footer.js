9b4c5b34dd4827bfee07cd3b02b811f90d96e2a7
Parker Morgan <jpmaster13@msn.com>
2021-03-10 17:30:15 -0700

    Added resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <footer className="my-4 p-4 bg-gray-600">
       <GitButton />
+      <a className="bg-green-900 rounded-lg p-2 m-2" href="https://docs.google.com/document/d/1qqWsuTGUr_tNZKjUqWaFZM9ZrQ5ajD-uNg9LgbvJ2ZE/edit?usp=sharing" target="_blank">Resume</a>
     </footer>
   );
 }
